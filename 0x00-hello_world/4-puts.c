933b94c87a21312c8eb1b20b886ffc793a8411ff
Dennis K. Dadey <deedadey@gmail.com>
2022-12-08 15:26:05 -0800

    this is C programing proper

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /*
  * main funciton = print a line using puts
+ *
  * Return value = 0
  * /
  int main(void) 
  {
  	puts("Programming is like building a multilingual puzzle");
 	return (0);
+	}
