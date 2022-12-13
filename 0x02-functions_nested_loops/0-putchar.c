2cf5a28f3d0556534edaa3ea8d4d6074effdb9e0
Dennis K. Dadey <deedadey@gmail.com>
2022-12-13 10:49:16 -0800

    _putchar lettering

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - will print _putchar\n
+ *Return: 0
+ */
+int main(void)
+{
+  _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('a');
+
+    return (0);
+    }
