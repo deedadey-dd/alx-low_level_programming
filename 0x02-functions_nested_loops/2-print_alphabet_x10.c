90d0c09f0d31e3664e25866b27e9def9f04b2f01
Dennis K. Dadey <deedadey@gmail.com>
2022-12-13 16:12:44 -0800

    task 2 alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10(void);
+	{
+		char ch;
+		int i;
+		for (i=0; i<10; i++)
+		{while (ch <= 'z')
+			_putchar(ch);
+			ch++;
+		}
+		_putchat('\n')
+	}
+	return (0);
+}
