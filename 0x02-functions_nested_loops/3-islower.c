ce6208ca1c8eac3596b9c7b5ac53e0446a1932e2
Dennis K. Dadey <deedadey@gmail.com>
2022-12-13 18:27:28 -0800

    task 03 corrected

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lowercase letters
  * @c: the alphabet to check
- * Retrun: 1 if c is lowercase, 0 if not
+ * Return: 1 if c is lowercase, 0 if not
  */
 
 int _islower(int c)
