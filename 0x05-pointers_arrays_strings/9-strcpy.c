9717d6630917908d5c1be62f6b4c0dad148c8ac3
Dennis K. Dadey <deedadey@gmail.com>
2022-12-19 16:18:04 -0800

    final commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string pointed to by @src, including the
+ * @dest: A temporal storage.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
