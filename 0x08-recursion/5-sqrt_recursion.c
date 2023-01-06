073badf0bd68579c779e345183ddbe50cb569309
Dennis Dadey <deedadey@gmail.com>
2023-01-05 16:43:49 -0800

    corrections of recursion tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,18 +4,25 @@ int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * actual_sqrt_recursion - recurse to find the natural
- * wquareroot of a number
  * @n: number to calculate the square root of
- * @i: iterator
  * Return: the resulting square root
  */
+int _sqrt_recursion(int n)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
+
+/**
+ * actual_sqrt_recursion - recurse to find the natural
+ * squareroot of a number
+ * @n: number to calculate the square root of
+ * @i: iterator
+ * Return: the resulting square root
+ */
+
 
 int actual_sqrt_recursion(int n, int i)
 {
