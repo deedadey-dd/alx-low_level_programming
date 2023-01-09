e40c248b804dd3bd9a9816b5f4c861152fc3cee7
Dennis Dadey <deedadey@gmail.com>
2023-01-09 09:11:41 -0800

    Arguments in C Programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * main - prints the name of program
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
+ * Return: 0 .
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc * 0]);
 
