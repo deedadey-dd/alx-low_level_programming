1413eb773bcc9755c2c156ffa654750538910da1
Dennis Dadey <deedadey@gmail.com>
2023-01-18 21:28:49 -0800

    Pointers to Functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  unsigned int index;
+	unsigned int index;
 
-  if (array != NULL && size > 0 && action != NULL)
-    for (index = 0; index < size; index++)
-      action(array[index]);
+	if (array != NULL && size > 0 && action != NULL)
+		for (index = 0; index < size; index++)
+			action(array[index]);
 }
